12fb2b4b0581c820c51251daf2b08995084235cb
wzh <scse1082@126.com>
2016-12-19 13:34:42 +0800

    search

--- app/src/main/java/com/example/think/myapplication/MainActivity.java
+++ app/src/main/java/com/example/think/myapplication/MainActivity.java
@@ -5,20 +5,58 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.*;
+
 
 import com.example.think.myapplication.structure.Dishes;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
     public static Dishes dishes;
+    private ImageView ivDeleteText;
+    private EditText  etSearch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        ivDeleteText = (ImageView) findViewById(R.id.ivDeleteText);
+        etSearch = (EditText) findViewById(R.id.etSearch);
+
+        ivDeleteText.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v) {
+                etSearch.setText("");
+            }
+        });
+
+        etSearch.addTextChangedListener(new TextWatcher() {
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // TODO Auto-generated method stub
+
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                // TODO Auto-generated method stub
+
+            }
+
+            public void afterTextChanged(Editable s) {
+                if (s.length() == 0) {
+                    ivDeleteText.setVisibility(View.GONE);
+                } else {
+                    ivDeleteText.setVisibility(View.VISIBLE);
+                }
+            }
+        });
         //if (dishes==null) dishes = new Dishes();
     }
 }
